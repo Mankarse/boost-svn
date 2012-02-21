74e23ca00afd45f07a3bf9bc672a973823ee4f35
Evan Wallace <onlyone@senet.com.au>
2012-02-22 03:47:39 +1100

    Added new test file
    This test file does not yet do anything.

---
+++ libs/variant/test/variant_move_test.cpp
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------------
+// boost-libs variant/test/variant_move_test.cpp source file
+// See http://www.boost.org for updates, documentation, and revision history.
+//-----------------------------------------------------------------------------
+//
+// Copyright (c) 2012
+// Evan Wallace
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "boost/variant.hpp"
+#include "boost/test/minimal.hpp"
+
+int test_main(int , char* [])
+{
+    return 0;
+}
